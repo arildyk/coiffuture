396b84e8a449ddb5734ad78870813c1cf9128ec6
Arild <65564205+arildyk@users.noreply.github.com>
2022-06-05 19:04:46 -0400

    Implementer la page titre

---
+++ js/index/index.js
@@ -0,0 +1,9 @@
+var searchAddon = document.getElementById("searchAddon");
+
+searchAddon.addEventListener("mouseover", function() {
+    this.style.backgroundColor = "#ff0000";
+});
+
+searchAddon.addEventListener("mouseout", function() {
+    this.style.backgroundColor = "#0000ff";
+});
