0b389c79e852c3eec5700664bf5b0b9b19066db9
Arild <65564205+arildyk@users.noreply.github.com>
2022-06-07 20:38:12 -0400

    Pages de réservations et pages de confirmation ajouté

---
+++ js/page-choisir-date/page-choisir-date.js
@@ -0,0 +1,12 @@
+var btnReturn = document.getElementById("btnReturn");
+var btnSubmit = document.getElementById("btnSubmit");
+
+btnReturn.onclick = function() {
+    window.location.href = "page-de-styles.html";
+}
+
+btnSubmit.onclick = function() {
+    window.location.href = "page-de-confirmation.html";
+}
+
+
--- js/page-du-style-choisi/page-du-style-choisi.js
+++ js/page-du-style-choisi/page-du-style-choisi.js
@@ -1,5 +1,20 @@
 var btnReturn = document.getElementById("btnReturn");
+var btnReserve1 = document.getElementById("btnReserve1");
+var btnReserve2 = document.getElementById("btnReserve2");
+var btnReserve3 = document.getElementById("btnReserve3");
 
 btnReturn.onclick = function() {
     window.location.href = "page-de-styles.html";
+}
+
+btnReserve1.onclick = function() {
+    window.location.href = "page-choisir-date-1.html";
+}
+
+btnReserve2.onclick = function() {
+    window.location.href = "page-choisir-date-2.html";
+}
+
+btnReserve3.onclick = function() {
+    window.location.href = "page-choisir-date-3.html";
 }
